19dba5bb63791a95bfb1a98b039781bc8cf81bc1
M1chal3k28 <balcerakm0@gmail.com>
2025-08-10 21:52:26 +0200

    Added new package for making elements draggable. Made question blocks draggable to add new functionality in the future like editing and deleteing question by draggin to sides.

--- src/Components/Menu/QuestionsPanel/QuestionBlock.tsx
+++ src/Components/Menu/QuestionsPanel/QuestionBlock.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Answer, Question } from "../../../Types";
 import "./QuestionPanel.css";
+import Draggable from "react-draggable";
 
 type QuestionBlockProps = {
     question: Question
@@ -13,19 +14,34 @@ const QuestionBlock: React.FC<QuestionBlockProps> = ({question}) => {
             <div className="w-4/12 text-right">{answer.score}🪙</div>
         </div>
     ));
+
+    const ref = React.useRef(null);
+
+    const handleStopDragging = (e: any) => {
+        console.log(e);
+    };
+
     return (
-        <div className="questionBlock menuSmallerTextSize">
-            <div className="flex justify-between mb-5">
-                <p className="w-10/12">💬 {question.question} 💬</p>
-                <p className="w-2/12 text-right"># {question.id}</p>
-            </div>
-            <hr className="border-2 rounded"/>
-            <div className="mt-5">
-                <div className="grid grid-cols-2 grid-flow-dense gap-6 w-full">
-                    {answers}
+        <Draggable
+        axis="x"
+        handle=".handle"
+        scale={1.01}
+        nodeRef={ref}
+        onStop={handleStopDragging}
+        bounds={{top: -100, left: -100, right: 100, bottom: 100}}>
+            <div ref={ref} className="questionBlock menuSmallerTextSize handle">
+                <div className="flex justify-between mb-5">
+                    <p className="w-10/12">💬 {question.question} 💬</p>
+                    <p className="w-2/12 text-right"># {question.id}</p>
+                </div>
+                <hr className="border-2 rounded"/>
+                <div className="mt-5">
+                    <div className="grid grid-cols-2 grid-flow-dense gap-6 w-full">
+                        {answers}
+                    </div>
                 </div>
             </div>
-        </div>
+        </Draggable>
     );
 };
 
