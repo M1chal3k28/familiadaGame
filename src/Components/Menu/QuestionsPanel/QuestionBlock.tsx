c67f2d0a36c3aa27776fdfdc26182dd691f68345
M1chal3k28 <balcerakm0@gmail.com>
2025-08-12 15:53:05 +0200

    Now questionBlock goes back to the starting postition after release. Added dragHandler that keep track of question block deltas. When question block is dragged to the sides of its parent container the propair (delete or edit) text is shown over the question block. Only visuals

--- src/Components/Menu/QuestionsPanel/QuestionBlock.tsx
+++ src/Components/Menu/QuestionsPanel/QuestionBlock.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { Answer, Question } from "../../../Types";
 import "./QuestionPanel.css";
-import Draggable from "react-draggable";
+import Draggable, { DraggableData, DraggableEvent } from "react-draggable";
 
 type QuestionBlockProps = {
     question: Question
 }
+
+const questionBlockDragBounds = {
+    top: -100,
+    right: 100,
+    bottom: 100,
+    left: -100
+};
 
 const QuestionBlock: React.FC<QuestionBlockProps> = ({question}) => {
     const answers = question.answers.map((answer: Answer, index) => (
@@ -15,20 +22,52 @@ const QuestionBlock: React.FC<QuestionBlockProps> = ({question}) => {
         </div>
     ));
 
-    const ref = React.useRef(null);
+    const ref = React.useRef<HTMLDivElement>(null);
+    const questionBlockDeleteRef = React.useRef<HTMLDivElement>(null);
+    const questionBlockEditRef = React.useRef<HTMLDivElement>(null);
+    const [ questionBlockPossiton, setQuestionBlockPossiton ] = React.useState({ x: 0, y: 0 });
+    const [ questionBlockDelta, setQuestionBlockDelta ] = React.useState({ x: 0, y: 0 });
 
-    const handleStopDragging = (e: any) => {
-        console.log(e);
+    const handleDrag = (_: DraggableEvent, ui: DraggableData) => {
+        const {x, y} = questionBlockDelta;
+        setQuestionBlockDelta({
+            x: x + ui.deltaX,
+            y: y + ui.deltaY,
+        });
+
+        if (questionBlockDelta.x <= questionBlockDragBounds.left * 0.75) {
+            questionBlockDeleteRef.current?.classList.remove("hidden");
+        } else if (questionBlockDelta.x >= questionBlockDragBounds.right * 0.75) {
+            questionBlockEditRef.current?.classList.remove("hidden");
+        } else {
+            questionBlockEditRef.current?.classList.add("hidden");
+            questionBlockDeleteRef.current?.classList.add("hidden");
+        }
     };
+
+    const handleStopDragging = (_: DraggableEvent) => {
+        setQuestionBlockPossiton({ x: 0, y: 0 });
+        setQuestionBlockDelta({ x: 0, y: 0 });
+
+        questionBlockDeleteRef.current?.classList.add("hidden");
+        questionBlockEditRef.current?.classList.add("hidden");
+    };
+
+    const handleStartDragging = (_: DraggableEvent) => {
+        () => _;
+    }
 
     return (
         <Draggable
         axis="x"
         handle=".handle"
-        scale={1.01}
+        scale={1}
         nodeRef={ref}
+        position={questionBlockPossiton}
         onStop={handleStopDragging}
-        bounds={{top: -100, left: -100, right: 100, bottom: 100}}>
+        onStart={handleStartDragging}
+        onDrag={handleDrag}
+        bounds={{top: questionBlockDragBounds.top, left: questionBlockDragBounds.left, right: questionBlockDragBounds.right, bottom: questionBlockDragBounds.bottom}}>
             <div ref={ref} className="questionBlock menuSmallerTextSize handle">
                 <div className="flex justify-between mb-5">
                     <p className="w-10/12">💬 {question.question} 💬</p>
@@ -39,6 +78,14 @@ const QuestionBlock: React.FC<QuestionBlockProps> = ({question}) => {
                     <div className="grid grid-cols-2 grid-flow-dense gap-6 w-full">
                         {answers}
                     </div>
+                </div>
+
+                <div ref={questionBlockEditRef} className="questionBlockOverlay hidden edit">
+                    EDIT
+                </div>
+
+                <div ref={questionBlockDeleteRef} className="questionBlockOverlay hidden delete">
+                    DELETE
                 </div>
             </div>
         </Draggable>
